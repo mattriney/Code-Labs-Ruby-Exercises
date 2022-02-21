c26daddf41b8153d90cdbb2c572eaea84a5ad009
mattriney <61891660+mattriney@users.noreply.github.com>
2022-02-20 20:49:05 -0600

    exercise 1.4

---
+++ part-1-exercises/exercise-1.4.rb
@@ -0,0 +1,9 @@
+def names_of_my_pets(name_1, name_2, name_3)
+    puts "I have 3 pets! Their names are #{name_1}, #{name_2}, and #{name_3}!"
+  end
+  
+  name_1 = "Max"
+  name_2 = "Jolene"
+  name_3 = "Gidget"
+  
+  names_of_my_pets(name_1, name_2, name_3)
